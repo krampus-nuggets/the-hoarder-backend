b529c3ebd46df86e963a993314734d0c15f288b4
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-01-23 02:19:51 +0200

    UPDATE -> project info comments

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/*
+Main Project: the-hoarder (https://github.com/krampus-nuggets/the-hoarder)
+This Project: the-hoarder-backend (https://github.com/krampus-nuggets/the-hoarder-backend)
+Relating Project: autotrader (https://github.com/krampus-nuggets/data_science/tree/master/autotrader)
+Author: @krampus-nuggets (https://github.com/krampus-nuggets)
+Role: Backend API
+*/
+
 // START - Imports
 const express = require("express");
 const autotrader = require("./modules/AutoTrader");
