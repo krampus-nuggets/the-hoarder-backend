835cd819c22b7fdc86fc9a9d9a144377a15bc9bc
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-01-23 02:06:57 +0200

    ADD -> server response to query string

--- modules/AutoTrader.js
+++ modules/AutoTrader.js
@@ -4,6 +4,15 @@ const express = require("express");
 const router = express.Router();
 // END
 
+    // START - Set server response
+    // Set JSON header to Response
+    res.header("Content-Type",'application/json');
+    // Prettify and Respond with carData[{JSON}]
+    res.send(JSON.stringify(carData, null, 4));
+    // End Response
+    res.end();
+    // END
+    
 
 // Export Module
 module.exports = router;
