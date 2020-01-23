b034c6d30c40ca3874c70e48bfd393b5d20a874d
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-01-23 02:07:35 +0200

    ADD -> GET handler & Puppeteer setup

--- modules/AutoTrader.js
+++ modules/AutoTrader.js
@@ -4,6 +4,17 @@ const express = require("express");
 const router = express.Router();
 // END
 
+// START - GET method for Puppeteer function @ Endpoint
+router.get("/", async function (req, res){
+    // Launch browser in headless mode
+    const browser = await puppeteer.launch({ headless: true });
+    // Create a new-tab within browser
+    const page = await browser.newPage();
+    // Set browser-viewport dimensions
+    await page.setViewport({ height: 1366, width: 768 });
+    // Go-To URL provided via Query String && waitUntil all traffic on network has stopped
+    await page.goto(req.query.url, { waitUntil: "networkidle0" });
+
     // START - Set server response
     // Set JSON header to Response
     res.header("Content-Type",'application/json');
